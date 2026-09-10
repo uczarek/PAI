7659a9e3e2c5cccee77b449300fbd20dc0dd17bc
Grzegorz Ujma <gujma@technikumlodz.pl>
2026-09-10 14:34:51 +0200

    sdfd

---
+++ zadanie 3 - php/index.php
@@ -0,0 +1,11 @@
+<?php
+    echo "testujemy"
+    $zmienna = 5;
+
+    if($zmienna > 4){
+        echo "zmienna wieksza od 4";
+    }else{
+        echo "zmienna mniejsza badz rowna 4";
+    }
+
+?>
